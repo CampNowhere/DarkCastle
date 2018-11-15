3b73e3babd119443c82008dbfc9241ff69e747b9
Karl Zander <pvial@Karls-MBP.local.tld>
2018-11-14 17:43:26 -0800

    Removal of broken RedDye

--- src/castle.c
+++ src/castle.c
@@ -6,7 +6,7 @@
 
 void usage() {
     printf("DarkCastle v0.1 - by KryptoMagik\n\n");
-    printf("Algorithms:\n***********\n\ndark64    256 bit\ndark      256 bit\nwrzeszcz  256 bit\nreddye    128 bit\nbluedye   256 bit\n\n");
+    printf("Algorithms:\n***********\n\ndark64    256 bit\ndark      256 bit\nwrzeszcz  256 bit\nbluedye   256 bit\n\n");
     printf("Usage: castle <algorithm> <-e/-d> <input file> <output file> <password>\n\n");
 }
 
--- src/castle_core.c
+++ src/castle_core.c
@@ -18,7 +18,7 @@ void dark_encrypt(char *infile_name, long fsize, char *outfile_name, int key_len
     infile = fopen(infile_name, "rb");
     outfile = fopen(outfile_name, "wb");
     unsigned char nonce[nonce_length];
-    reddye_random(&nonce, nonce_length);
+    wrzeszcz_random(&nonce, nonce_length);
     fwrite(nonce, 1, nonce_length, outfile);
     fread(msg, 1, fsize, infile);
     dark_crypt(msg, key, nonce, fsize);
@@ -87,7 +87,7 @@ void dark64_encrypt(char *infile_name, long fsize, char *outfile_name, int key_l
     infile = fopen(infile_name, "rb");
     outfile = fopen(outfile_name, "wb");
     unsigned char nonce[nonce_length];
-    reddye_random(&nonce, nonce_length);
+    wrzeszcz_random(&nonce, nonce_length);
     fwrite(nonce, 1, nonce_length, outfile);
     fread(msg, 1, fsize, infile);
     dark64_crypt(msg, key, nonce, fsize);
@@ -145,75 +145,6 @@ void dark64_decrypt(char *infile_name, long fsize, char *outfile_name, int key_l
     }
 }
 
-void reddye_encrypt(char *infile_name, long fsize, char *outfile_name, int key_length, int nonce_length, int mac_length, int kdf_iterations, unsigned char * kdf_salt, unsigned char *password) { 
-    FILE *infile, *outfile;
-    unsigned char *msg;
-    msg = (unsigned char *) malloc(fsize);
-    unsigned char *mac[mac_length];
-    unsigned char *mac_key[key_length];
-    unsigned char *key[key_length];
-    ganja_kdf(password, strlen(password), key, kdf_iterations, key_length, kdf_salt);
-    infile = fopen(infile_name, "rb");
-    outfile = fopen(outfile_name, "wb");
-    unsigned char nonce[nonce_length];
-    reddye_random(&nonce, nonce_length);
-    fwrite(nonce, 1, nonce_length, outfile);
-    fread(msg, 1, fsize, infile);
-    reddye_crypt(msg, key, nonce, fsize, key_length, nonce_length);
-    fwrite(msg, 1, fsize, outfile);
-    fclose(outfile);
-    fclose(infile);
-    free(msg);
-
-    outfile = fopen(outfile_name, "rb");
-    fseek(outfile, 0, SEEK_END);
-    fsize = ftell(outfile);
-    fseek(outfile, 0, SEEK_SET);
-    msg = (unsigned char *) malloc(fsize);
-    fread(msg, 1, fsize, outfile);
-    fclose(outfile);
-    outfile = fopen(outfile_name, "wb");
-    ganja_kdf(key, key_length, mac_key, kdf_iterations, key_length, kdf_salt);
-    h4a_mac(msg, fsize, mac, mac_key, key_length);
-    fwrite(mac, 1, mac_length, outfile);
-    fwrite(msg, 1, fsize, outfile);
-    fclose(outfile);
-    free(msg);
-}
-
-void reddye_decrypt(char *infile_name, long fsize, char *outfile_name, int key_length, int nonce_length, int mac_length, int kdf_iterations, unsigned char * kdf_salt, unsigned char *password) {
-    FILE *infile, *outfile;
-    unsigned char *mac[mac_length];
-    unsigned char *mac_key[key_length];
-    unsigned char *key[key_length];
-    unsigned char *msg;
-    unsigned char *mac_verify[mac_length];
-    infile = fopen(infile_name, "rb");
-    msg = (unsigned char *) malloc(fsize-mac_length);
-    ganja_kdf(password, strlen(password), key, kdf_iterations, key_length, kdf_salt);
-    ganja_kdf(key, key_length, mac_key, kdf_iterations, key_length, kdf_salt);
-    fread(mac, 1, mac_length, infile);
-    fread(msg, 1, (fsize-mac_length), infile);
-    h4a_mac(msg, (fsize-mac_length), mac_verify, mac_key, key_length);
-    free(msg);
-    if (memcmp(mac, mac_verify, mac_length) == 0) {
-        msg = (unsigned char *) malloc(fsize-mac_length-nonce_length);
-        unsigned char *nonce[nonce_length];
-        fseek(infile, mac_length, SEEK_SET);
-        fread(nonce, 1, nonce_length, infile);
-        fread(msg, 1, (fsize - mac_length - nonce_length), infile);
-        fclose(infile);
-        reddye_crypt(msg, key, nonce, (fsize - mac_length - nonce_length), key_length, nonce_length);
-        outfile = fopen(outfile_name, "wb");
-        fwrite(msg, 1, (fsize - mac_length - nonce_length), outfile);
-        fclose(outfile);
-        free(msg);
-    }
-    else {
-        printf("Error: Message has been tampered with.\n");
-    }
-}
-
 void bluedye_encrypt(char *infile_name, long fsize, char *outfile_name, int key_length, int nonce_length, int mac_length, int kdf_iterations, unsigned char * kdf_salt, unsigned char *password) { 
     FILE *infile, *outfile;
     unsigned char *msg;
@@ -225,7 +156,7 @@ void bluedye_encrypt(char *infile_name, long fsize, char *outfile_name, int key_
     infile = fopen(infile_name, "rb");
     outfile = fopen(outfile_name, "wb");
     unsigned char nonce[nonce_length];
-    reddye_random(&nonce, nonce_length);
+    wrzeszcz_random(&nonce, nonce_length);
     fwrite(nonce, 1, nonce_length, outfile);
     fread(msg, 1, fsize, infile);
     bluedye_crypt(msg, key, nonce, fsize, key_length, nonce_length);
@@ -294,7 +225,7 @@ void wrzeszcz_encrypt(char *infile_name, long fsize, char *outfile_name, int key
     infile = fopen(infile_name, "rb");
     outfile = fopen(outfile_name, "wb");
     unsigned char nonce[nonce_length];
-    reddye_random(&nonce, nonce_length);
+    wrzeszcz_random(&nonce, nonce_length);
     fwrite(nonce, 1, nonce_length, outfile);
     fread(msg, 1, fsize, infile);
     wrzeszcz_crypt(msg, key, nonce, fsize, key_length, nonce_length);
