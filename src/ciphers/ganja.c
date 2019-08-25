29172e89d909713c52aa778ae01783a9e384d485
Karl Zander <pvial@Karls-MacBook-Air.local>
2019-08-25 00:18:32 -0700

    v0.1.9 - improved hmac

--- src/ciphers/ganja.c
+++ src/ciphers/ganja.c
@@ -126,22 +126,21 @@ unsigned char * ganja_digest(unsigned char * data, long datalen, unsigned char *
 unsigned char * ganja_hmac(unsigned char * data, long datalen, unsigned char * D, unsigned char * key, int keylen, unsigned char *salt) {
     unsigned char *a[32];
     unsigned char *b[32];
-    unsigned char c[32];
-    unsigned char k[200];
+    unsigned char *c[32];
+    unsigned char *k[keylen*4];
     int i;
-    memset(k, key, keylen);
-    ganja_digest(data, datalen, a, salt, strlen(salt));
-    ganja_digest(k, keylen, b, salt, strlen(salt));
-    for (i = 0; i < 32; i++) {
-        c[i] = (uintptr_t)a[i] ^ (uintptr_t)b[i];
-    }
-    ganja_digest(a, keylen, D, salt, strlen(salt));
+    //memset(k, key, keylen);
+    ganja_digest(data, datalen, D, key, keylen);
+    //for (i = 0; i < 32; i++) {
+    //    c[i] = (uintptr_t)a[i] ^ (uintptr_t)key[i];
+    //}
+    //ganja_digest(c, 32, D, salt, strlen(salt));
 }
 
 unsigned char * ganja_kdf(unsigned char * password, int passlen, unsigned char * D, int iterations, int keylen, unsigned char *salt) {
     int b, i, f, s, r;
     int c = 0;
-    ganja_digest(password, strlen(password), D, salt, 12);
+    ganja_digest(password, passlen, D, salt, 12);
     for (i = 0; i < iterations; i++) {
         ganja_digest(D, keylen, D, salt, 12);
     }
