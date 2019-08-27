53c4ddb033e35b427bf865835dad94745ce62947
Karl Zander <pvial@Karls-MacBook-Air.local>
2019-08-26 20:15:18 -0700

    DarkCastle/DarkPass test suite

---
+++ src/scripts/testcastle.py
@@ -0,0 +1,52 @@
+import subprocess, os, time, sys
+
+#This script assumes that you have the castle, darkpass and ent binaries installed
+
+inputfile = "testfile1"
+testfile_size = 1048576
+# must be between 0 and 255
+test_char = 0
+
+def getkey(length):
+    cmd = ['darkpass', length]
+    k = subprocess.check_output(cmd)
+    return k
+
+def geninputfile():
+    f = open(inputfile, "w")
+    buf = []
+    for x in xrange(testfile_size):
+        buf.append(chr(test_char))
+    f.write("".join(buf))
+    f.close()
+
+if (len(sys.argv) != 3):
+    print "python testcastle.py <algorithm> <password length>"
+    exit(1)
+
+try:
+    alg = sys.argv[1]
+except IndexError as ier:
+    print "Algorithm is missing"
+try:
+    passlen = sys.argv[2]
+except IndexError as ier:
+    print "Password length is missing"
+
+geninputfile()
+
+while True:
+    key = getkey(passlen)
+    print key
+    cmdenc = ['castle', alg, '-e', inputfile, 'b1' , key]
+    out = subprocess.check_output(cmdenc)
+    print "enc" + out
+    cmddec = ['castle', alg, '-d', 'b1','b2' , key]
+    out = subprocess.check_output(cmddec)
+    print "dec" + out
+    cmdent = ['ent', 'b2']
+    out = subprocess.check_output(cmdent)
+    lines = out.split("\n")
+    l = lines[0].split("=")[1].strip()
+    print l
+    time.sleep(0.5)
