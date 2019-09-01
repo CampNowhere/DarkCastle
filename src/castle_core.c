0f4311035c65f3c0f6c8c66601461e44cf8e88de
Karl Zander <pvial@Karls-MacBook-Air.local>
2019-09-01 12:53:19 -0700

    v0.3.3 - add Amaugs 1024 and 1024 key wrapping

--- src/castle_core.c
+++ src/castle_core.c
@@ -1011,3 +1011,79 @@ void amagus512_decrypt(char *infile_name, long long fsize, char *outfile_name, i
         printf("Error: Message has been tampered with.\n");
     }
 }
+
+void amagus1024_encrypt(char *infile_name, long long fsize, char *outfile_name, int key_length, int nonce_length, int mac_length, int kdf_iterations, unsigned char * kdf_salt, unsigned char *password) { 
+    FILE *infile, *outfile;
+    unsigned char *msg;
+    msg = (unsigned char *) malloc(fsize);
+    unsigned char mac[mac_length];
+    unsigned char mac_key[key_length];
+    unsigned char key[key_length];
+    unsigned char *keyprime[key_length];
+    unsigned char *K[key_length];
+    manja_kdf(password, strlen(password), key, key_length, kdf_salt, strlen(kdf_salt), kdf_iterations);
+    infile = fopen(infile_name, "rb");
+    outfile = fopen(outfile_name, "wb");
+    key_wrap_1024_encrypt(keyprime, key_length, key, K);
+    unsigned char nonce[nonce_length];
+    wrzeszcz_random(&nonce, nonce_length);
+    fwrite(nonce, 1, nonce_length, outfile);
+    fwrite(K, 1, key_length, outfile);
+    fread(msg, 1, fsize, infile);
+    amagus_crypt(msg, keyprime, key_length, nonce, fsize);
+    fwrite(msg, 1, fsize, outfile);
+    fclose(outfile);
+    fclose(infile);
+    free(msg);
+
+    outfile = fopen(outfile_name, "rb");
+    fseek(outfile, 0, SEEK_END);
+    fsize = ftell(outfile);
+    fseek(outfile, 0, SEEK_SET);
+    msg = (unsigned char *) malloc(fsize);
+    fread(msg, 1, fsize, outfile);
+    fclose(outfile);
+    outfile = fopen(outfile_name, "wb");
+    manja_kdf(key, key_length, mac_key, key_length, kdf_salt, strlen(kdf_salt), kdf_iterations);
+    ganja_hmac(msg, fsize, &mac, mac_key, key_length, kdf_salt);
+    fwrite(mac, 1, mac_length, outfile);
+    fwrite(msg, 1, fsize, outfile);
+    fclose(outfile);
+    free(msg);
+}
+
+void amagus1024_decrypt(char *infile_name, long long fsize, char *outfile_name, int key_length, int nonce_length, int mac_length, int kdf_iterations, unsigned char * kdf_salt, unsigned char *password) {
+    FILE *infile, *outfile;
+    unsigned char mac[mac_length];
+    unsigned char mac_key[key_length];
+    unsigned char key[key_length];
+    unsigned char *keyprime[key_length];
+    unsigned char *msg;
+    unsigned char mac_verify[mac_length];
+    infile = fopen(infile_name, "rb");
+    msg = (unsigned char *) malloc(fsize-mac_length);
+    manja_kdf(password, strlen(password), key, key_length, kdf_salt, strlen(kdf_salt), kdf_iterations);
+    manja_kdf(key, key_length, mac_key, key_length, kdf_salt, strlen(kdf_salt), kdf_iterations);
+    fread(&mac, 1, mac_length, infile);
+    fread(msg, 1, (fsize-mac_length), infile);
+    ganja_hmac(msg, (fsize-mac_length), &mac_verify, mac_key, key_length, kdf_salt);
+    free(msg);
+    if (memcmp(mac, mac_verify, mac_length) == 0) {
+        msg = (unsigned char *) malloc(fsize-mac_length-nonce_length-key_length);
+        unsigned char *nonce[nonce_length];
+        fseek(infile, mac_length, SEEK_SET);
+        fread(nonce, 1, nonce_length, infile);
+        fread(keyprime, 1, key_length, infile);
+        key_wrap_1024_decrypt(keyprime, key_length, key);
+        fread(msg, 1, (fsize - mac_length - nonce_length-key_length), infile);
+        fclose(infile);
+        amagus_crypt(msg, keyprime, key_length,  nonce, (fsize - mac_length - nonce_length - key_length));
+        outfile = fopen(outfile_name, "wb");
+        fwrite(msg, 1, (fsize - mac_length - nonce_length - key_length), outfile);
+        fclose(outfile);
+        free(msg);
+    }
+    else {
+        printf("Error: Message has been tampered with.\n");
+    }
+}
--- src/crypto_funcs.c
+++ src/crypto_funcs.c
@@ -3,6 +3,22 @@
 #include <string.h>
 #include <unistd.h>
 
+void * key_wrap_1024_encrypt(unsigned char * keyprime, int key_length, unsigned char * key, unsigned char * K) {
+    wrzeszcz_random(keyprime, key_length);
+    memcpy(K, keyprime, key_length);
+    amagus_crypt(K, key, keylen, key, key_length);
+    for (int i = 0; i < key_length; i++) {
+        K[i] = K[i] ^ key[i];
+    }
+}
+
+void * key_wrap_1024_decrypt(unsigned char * keyprime, int key_length, unsigned char * key) {
+    for (int i = 0; i < key_length; i++) {
+        keyprime[i] = keyprime[i] ^ key[i];
+    }
+    amagus_crypt(keyprime, key, keylen, key, key_length);
+}
+
 void * key_wrap_512_encrypt(unsigned char * keyprime, int key_length, unsigned char * key, unsigned char * K) {
     wrzeszcz_random(keyprime, key_length);
     memcpy(K, keyprime, key_length);
